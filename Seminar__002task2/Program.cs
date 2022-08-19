fcd6c4f907af2aff615dd1358f51aad6c8ad9291
Artem <artem-vishnevskiy-00@mail.ru>
2022-08-19 12:53:59 +0300

    2Example

---
+++ Seminar__002task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if(n == b * b)
+
+{
+    Console.WriteLine("да");
+}
+
+else
+{
+    Console.WriteLine("нет");
+}
