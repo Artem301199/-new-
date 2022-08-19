a5088f37383d779ef68a28c13440af785b873b5b
Artem <artem-vishnevskiy-00@mail.ru>
2022-08-19 13:29:06 +0300

    нахождение квадрата

---
+++ Seminar__001TAsk3/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Введите любое число от 1 до 7");
+int n = Convert.ToInt32(Console.ReadLine());
+switch (n)
+{
+case 1:
+{
+    Console.WriteLine("понедельник");
+break;
+}
+case 2:
+{
+    Console.WriteLine("Вторник");
+    break;
+}
+case 3:
+{
+    Console.WriteLine("среда");
+break;
+}
+case 4:
+{
+    Console.WriteLine("четверг");
+break;
+}
+case 5:
+{
+    Console.WriteLine("пятница");
+break;
+}
+case 6:
+{
+    Console.WriteLine("суббота");
+break;
+}
+case 7:
+{
+    Console.WriteLine("Восрексенье");
+break;
+}
+} 
+
